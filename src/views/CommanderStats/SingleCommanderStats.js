c2acdf210da3bdbb4f9e4f335757149def2485d0
jakboa <jakob.boaz@mac.com>
2024-12-20 21:09:40 +0100

    v0.10.8 Minor Layout
     - Changed the layout in SingleCommanderStats.

--- src/views/CommanderStats/SingleCommanderStats.js
+++ src/views/CommanderStats/SingleCommanderStats.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import SearchHandler from "../../components/SearchHandler";
-import Container  from "react-bootstrap/Container";
 import MatchInfoBox from "../../components/MatchInfo/MatchInfoBox";
 import EntityScore from "../../components/EntityScore";
 import LineChart from "../../components/charts/LineChart";
+import DoughnutChart from "../../components/charts/DoughnutChart";
+
+import Col  from "react-bootstrap/Col";
+import Row  from "react-bootstrap/Row";
+
 
 export default function SingleCommanderStats() {
 
@@ -13,12 +17,31 @@ export default function SingleCommanderStats() {
     const [ matchResultsForCommander ] = useState(SearchHandler.getEntityResults(commanderName,commanderInfo))
     
     return (
-        <Container>
-            <p>{commanderName}</p>
-            <MatchInfoBox matchDetails={ commanderInfo } />
-            <EntityScore results={ matchResultsForCommander } totalGames={ commanderInfo.length } />
-            <LineChart entityName={ commanderName } entityMatches={ commanderInfo } />
-        </Container>
+        <Row>
+            <Col md={12}>
+                <p>{commanderName}</p>
+            </Col>
+            <Col md={4} >
+                <p>Her er commander info.</p>
+                <p>INSERT INFO HERE:</p>
+            </Col>
+            <Col md={8}>
+                <Row className="border border-4 border-black">
+                    <Col md={3} className="border border-5 border-white bg-light-subtle p-3 m-3 rounded-5">
+                        <EntityScore results={ matchResultsForCommander } totalGames={ commanderInfo.length } />
+                    </Col>
+                    <Col md={8} className="border border-5 border-white bg-light-subtle p-3 m-3 rounded-5">
+                        <DoughnutChart />
+                    </Col>
+                    <Col md={11} className="border border-5 border-white bg-light-subtle p-3 m-3 rounded-5">
+                        <LineChart entityName={ commanderName } entityMatches={ commanderInfo } />
+                    </Col>
+                </Row>
+            </Col>
+            <Col md={12} className="border border-5 border-black round-end">
+                <MatchInfoBox matchDetails={ commanderInfo } />
+            </Col>
+        </Row>
 
     )
 }
