9c6ee5376b5d0506addb94a2cef1780832bb86ab
jakboa <jakob.boaz@mac.com>
2025-02-16 10:15:44 +0100

    v0.18.25 General Update
    - More UI edditing on EntityChart.

--- src/components/EntityScore.js
+++ src/components/EntityScore.js
@@ -16,9 +16,11 @@ export default function EntityScore( { matchResultsForEntity, totalGames, years 
     },{1:0,2:0,3:0,4:0}); 
 
     return (
-        <div className="d-flex flex-column h-100 bg-light-subtle border border-white rounded-5 ">
-            <h2 className="mt-1">Match Results:</h2>
-            <div className="d-flex flex-column flex-grow-1 mx-2 mb-3  fs-4 fw-medium">
+        <div className="d-flex flex-column h-100 bg-light-subtle border border-white rounded-4 ">
+            <div className="d-flex justify-content-center align-items-end">
+                <h2 className="mt-1 rounded-top  px-2 mb-0 mt-2" style={{background:"#78B2F4"}} >Match Results:</h2>
+            </div>
+            <div className="d-flex flex-column flex-grow-1 mx-3 mb-3  fs-4 fw-medium">
             { Object.values(allResults).map((results, placement) => {
                 return (
                     <div key={placement} className={`entityScore${placement+1} flex-grow-1 d-flex justify-content-center align-items-center`}>
--- src/components/charts/DoughnutChart.js
+++ src/components/charts/DoughnutChart.js
@@ -39,7 +39,7 @@ export default function DoughnutChart( { results, years } ) {
         },},
       },
     },
-    events:['click'],
+    
   };
 
 const data = {
