4ca2f46ac4a455181a86fb216209a9a6b746af6a
jakboa <jakob.boaz@mac.com>
2025-03-07 19:52:51 +0100

    v0.18.38 General Update
    - More UI added to the Cards on the homepage.

--- src/views/Homepage/HomepageGroupSingle.js
+++ src/views/Homepage/HomepageGroupSingle.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+//import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 
 export default function HomepageSingleGroup( {groupName, groupPlayers} ) {
+
+    const [mouseEnter, setMouseEnter] = useState(null);
+    const [mouseEnterPlayer, setMouseEnterPlayer] = useState(null);
 
     const navigate = useNavigate();
 
@@ -19,21 +22,21 @@ export default function HomepageSingleGroup( {groupName, groupPlayers} ) {
     //<Card.Header className="fs-5 fw-bold rounded-0">{groupName}</Card.Header>
 
     return (
-        <Card className="rounded-0 shadow cardCSS" style={{ width: '18rem' }} onClick={handleGroupClick}>
-            <Card.Body className="">
-                <Card.Text className="fw-bold fs-3 py-0 my-0">{groupName}</Card.Text>
-                <Card.Text className="fw-bold py-0 my-0">Players:</Card.Text>
-                <Row>
+        <Card className={`rounded-0 shadow cardCSS ${mouseEnter === groupName ? "bg-light fw-bold" : "" }`} style={{ width: '18rem' }} onClick={handleGroupClick} onMouseEnter={()=> setMouseEnter(groupName)} onMouseLeave={()=> setMouseEnter(null)}>
+            <Card.Header className="fs-3 py-0 mt-0 mb-2">{groupName}</Card.Header>
+            <Card.Body className="d-flex flex-column">
+                <Row className="border rounded border-5 flex-fill">
                 {groupPlayers[0].map((player,index) => {
                     return( 
-                        <Col md={6} key={index} className="border rounded border-5 bg-success fw-bold">
-                            <Card.Text className="py-0 my-0">{player}</Card.Text>
+                        <Col md={6} key={index} className=  {`border rounded bg-success d-flex justify-content-center align-items-center ${mouseEnterPlayer === index ? "bg-light fw-bold" : "" }`}
+                            onMouseEnter={()=> setMouseEnterPlayer(index)} onMouseLeave={()=> setMouseEnterPlayer(null)}>
+                            <Card.Text className="">{player}</Card.Text>
                         </Col>
                     )
                 })}
                 </Row>
-                <Button className="mt-3 fw-bold" variant="info" onClick={handleGroupClick}>Click for Group Stats!</Button>
             </Card.Body>
+            <Card.Footer>XX Matches</Card.Footer>
         </Card>
 
     );
