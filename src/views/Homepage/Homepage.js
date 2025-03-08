7ae0b3c931b2023cd56a9f3f69ac40f727d7aefe
jakboa <jakob.boaz@mac.com>
2025-03-08 12:27:07 +0100

    v0.18.38.01 Small edit
    - Fixing eslint issues so that i can publish to Azure.

--- src/views/Homepage/Homepage.js
+++ src/views/Homepage/Homepage.js
@@ -3,7 +3,7 @@ import React, { useReducer } from "react";
 import HomepageGroups from "./HomepageGroups";
 import HomepagePlayers from "./HomepagePlayers";
 import HomepageCommanders from "./HomepageCommanders";
-import HomepageIntro from "./HomepageIntro";
+//import HomepageIntro from "./HomepageIntro";
 import SearchHandler from '../../components/SearchHandler';
 import Header from "../../components/header/Header";
 
