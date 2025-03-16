12e81bbb4018237d78b3df66b68dfb5e2c4e2238
jakboa <jakob.boaz@mac.com>
2025-03-16 11:28:41 +0100

    v0.19.5 Database Update
     - Added timeout for the search in order to not call the api to much.

--- src/views/GroupStats/AddGroupMatch.js
+++ src/views/GroupStats/AddGroupMatch.js
@@ -19,25 +19,31 @@ export default function AddGroupMatch(props) {
     const valueControll = [1,2,3,4];
     //Bad hardcoded values, very bad!
     const yearDisplay = ["2021","2022","2023","2024","2025"];
+    // These two are used for delaying the fetch of commanders so its not to greedy, 
+    // can edit the milliseconds with timeOutTime.
+    let timeOutId;
+    const timeOutTime = 800;
 
     console.log(matchResults);
 
 
     // This function deals with finding the commander each player used for the match.
-    const getOptions = async (searchValue, callback) => {
-
+    const getOptions = async (searchValue) => {
+        try {
         // First get all cards that fitt the name.
         const cardOptions = await ScryFallAPIConnector.getAutoCompleteCommander(searchValue);
+        
 
-        if (!cardOptions?.length) return callback([]);
+        if (!cardOptions?.length) return [];
 
         // Make the result fitt the get bulk card API call. 
-        //Personal note: () in map is used to say its a object + name will be the key and the string the value.
+        //OLD Personal note: () in map is used to say its a object + name will be the key and the string the value.
         const formatedCardSearch = cardOptions.map(name => {
             const frontFace = name.includes("//") ? name.split(" // ")[0] : name;
             return { name:frontFace };
         });
-        // Get info about the cards that fitt.
+        //console.log("HERE",formatedCardSearch)
+        // Get info about the cards that fit.
         const cardInfo = await ScryFallAPIConnector.getGroupCommanderData(formatedCardSearch);
         const filterRegex = /Legendary.*(Creature|Planeswalker)/i
         const filteredCommanders = cardInfo.data.filter(card => filterRegex.test(card.type_line));
@@ -53,7 +59,13 @@ export default function AddGroupMatch(props) {
         
         console.log("getOptions", searchValue,cardOptions,formatedCardSearch,cardInfo,filteredCommanders, finalShow);
 
-        callback(finalShow);
+        return finalShow;
+
+    } catch (error) {
+        console.error("ScryFall API Error:", error.response || error.message || error);
+        console.log("Error fetching data from ScryFall:", error);
+        return [];
+    }
           
     };
 
@@ -70,7 +82,6 @@ export default function AddGroupMatch(props) {
     };
 
     const handlePlacementChange = (e, index) => {
-        console.log(typeof(e.target.value));
         setMatchResults((prev) => {
             const updatedPlacement = [...matchResults.results];
             updatedPlacement[index].placement = Number(e.target.value);
@@ -100,19 +111,19 @@ export default function AddGroupMatch(props) {
     return (
         <Modal {...props} centered size="lg">
             <Modal.Header closeButton>
-                <Modal.Title>Add a match to record!</Modal.Title>
+                <Modal.Title>REGISTER NEW MATCH.</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h4>{props.groupname}</h4>
-                <Form>
+                <h4>Group: {props.groupname}</h4>
+                <Form className="d-flex flex-column">
                     <table className="w-100">
                         <colgroup>
-                        <col style={{width:'1%'}} />
-                        <col style={{width:'40%'}} />
-                        <col style={{width:'50%'}} />
+                            <col style={{width:'10%'}} />
+                            <col style={{width:'40%'}} />
+                            <col style={{width:'50%'}} />
                         </colgroup>
                         <thead>
-                            <tr>
+                            <tr className="text-center">
                                 <th>Placement</th>
                                 <th>Player</th>
                                 <th>Commander</th>
@@ -122,7 +133,7 @@ export default function AddGroupMatch(props) {
                             {props.group.arrayPlayerNicks.map((_, index) => {
                                 return (
                                     <tr key={index} className={`place${matchResults.results[index].placement-1}`}>
-                                        <td className="py-2">
+                                        <td className="py-2 ps-2">
                                             <Form.Select onChange={(e) => handlePlacementChange(e,index)}
                                                 value={matchResults.results[index].placement}>
                                                 {valueControll.map((option, index) => {
@@ -135,8 +146,14 @@ export default function AddGroupMatch(props) {
 
                                         <td className="text-center">{matchResults.results[index].playerNick}</td>
                                         
-                                        <td>
-                                            <AysncSelect loadOptions={getOptions}
+                                        <td className="pe-2">
+                                            <AysncSelect loadOptions={(inputValue,callback) => {
+                                                clearTimeout(timeOutId);
+                                                timeOutId = setTimeout(async () => {
+                                                    const options = await getOptions(inputValue);
+                                                    callback(options);
+                                                },timeOutTime)
+                                            }}
                                                 value={
                                                     {value:matchResults.results[index].commander,
                                                      label:matchResults.results[index].commander
@@ -149,13 +166,17 @@ export default function AddGroupMatch(props) {
                             })}
                         </tbody>
                     </table>
-                    <Form.Select onChange={(e) => handleYearChange(e)}>
-                        {yearDisplay.map((option, index) => {
-                            return (
-                                <option key={index} value={option}>{option}</option>
-                                )
-                        })}
-                    </Form.Select>
+                    <Form.Group className="place2 my-2 p-2 rounded d-inline-flex  mx-auto justify-content-center">
+                        <Form.Label className="mt-2 text-end me-1">Select year played:</Form.Label>
+                        <Form.Select className="w-auto"
+                            onChange={(e) => handleYearChange(e)}>
+                            {yearDisplay.map((option, index) => {
+                                return (
+                                    <option key={index} value={option}>{option}</option>
+                                    )
+                            })}
+                        </Form.Select>
+                    </Form.Group>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
